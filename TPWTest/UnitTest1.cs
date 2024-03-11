de97063949b09a41dce5d0b89f6a5bf9bf65f368
247638 Centkowski Łukasz <247638@edu.p.lodz.pl>
2024-03-11 16:39:56 +0100

    pierwszy test 1=1

---
+++ TPWTest/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace TPWTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1() {
+            Assert.AreEqual(1, 1); 
+        
+        }
+    }
+}
